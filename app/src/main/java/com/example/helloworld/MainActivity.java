8056b2bded29d36cec369604e80d42bee161be5e
morestydy <1692125978@qq.com>
2020-04-09 18:28:07 +0800

    Android_Learning

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTextView;
+    private Button mBtnButton;
+    private Button mBtnEditText;
+    private Button mBtnEditEx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnTextView = findViewById(R.id.btn_textview);
+        mBtnButton = findViewById(R.id.btn_button);
+        mBtnEditText = findViewById(R.id.btn_EditText);
+        mBtnEditEx = findViewById(R.id.btn_EditEx);
+        setListeners();
+    }
+    protected void setListeners(){
+        OnClick onclick = new OnClick();
+        mBtnTextView.setOnClickListener(onclick);
+        mBtnButton.setOnClickListener(onclick);
+        mBtnEditText.setOnClickListener(onclick);
+        mBtnEditEx.setOnClickListener(onclick);
+
+    }
+
+    private class OnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()){
+                case R.id.btn_textview:
+                    intent = new Intent(MainActivity.this, TextViewActivity.class);
+                    break;
+                case R.id.btn_button:
+                    intent = new Intent(MainActivity.this, ButtonActivity.class);
+                    break;
+                case R.id.btn_EditText:
+                    intent = new Intent(MainActivity.this, EditTextActivity.class);
+                    break;
+                case R.id.btn_EditEx:
+                    intent = new Intent(MainActivity.this, EditExActivity.class);
+                    break;
+                default:
+                    break;
+            }
+            startActivity(intent);
+        }
     }
 }
